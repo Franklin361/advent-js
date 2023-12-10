5c39d52176f4c2a31712bff23a89260d04a26cfc
Franklin Martinez Lucas <frankomtzlucas361@gmail.com>
2023-12-10 10:26:51 -0600

    add alternatives reto 09

--- advent-js-2023/reto-09/index.ts
+++ advent-js-2023/reto-09/index.ts
@@ -29,3 +29,25 @@ function alternative01(lights: string[]): number {
 
   return Math.min(res, lights.length - res);
 }
+
+// 02
+// (190 pts) | 🚀 2442 ops/s | 🧐 Complejidad cognitiva: 11
+function alternative02(lights: string[]): number {
+  const patron1 = Array(lights.length)
+    .fill('🔴')
+    .map((v, i) => (i % 2 ? '🟢' : v));
+  const patron2 = Array(lights.length)
+    .fill('🟢')
+    .map((v, i) => (i % 2 ? '🔴' : v));
+
+  let c1 = 0;
+  let c2 = 0;
+  lights.forEach((v, i) => {
+    if (v !== patron1.at(i)) {
+      c1++;
+    } else if (v !== patron2.at(i)) {
+      c2++;
+    }
+  });
+  return c1 < c2 ? c1 : c2;
+}
